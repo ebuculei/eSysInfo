aaeb9b9cc9b53d73219e18e3c64d1621ba6489bc
Buculei <ebuculei@msn.com>
2022-09-26 18:36:28 -0700

    Add files via upload

---
+++ eSysInfo/AboutBox.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace eSysInfo
+{
+    partial class AboutBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutBox));
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.okButton = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.ColumnCount = 1;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel.Controls.Add(this.tableLayoutPanel1, 0, 3);
+            this.tableLayoutPanel.Controls.Add(this.label4, 0, 0);
+            this.tableLayoutPanel.Controls.Add(this.okButton, 0, 4);
+            this.tableLayoutPanel.Controls.Add(this.richTextBox1, 0, 2);
+            this.tableLayoutPanel.Controls.Add(this.label3, 0, 1);
+            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel.Location = new System.Drawing.Point(14, 14);
+            this.tableLayoutPanel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 6;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 31F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 77F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 39F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 31F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(452, 207);
+            this.tableLayoutPanel.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 72F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 228F));
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.linkLabel1, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(4, 139);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(300, 22);
+            this.tableLayoutPanel1.TabIndex = 27;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 0);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Email:";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(76, 0);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(148, 20);
+            this.linkLabel1.TabIndex = 1;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "ebuculei@msn.com";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label4
+            // 
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Location = new System.Drawing.Point(9, 0);
+            this.label4.Margin = new System.Windows.Forms.Padding(9, 0, 4, 0);
+            this.label4.MaximumSize = new System.Drawing.Size(0, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(439, 26);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "System Info 1.0.0.0";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // okButton
+            // 
+            this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.okButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.okButton.Location = new System.Drawing.Point(336, 171);
+            this.okButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(112, 29);
+            this.okButton.TabIndex = 24;
+            this.okButton.Text = "&OK";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Location = new System.Drawing.Point(9, 62);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(9, 5, 4, 5);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(439, 66);
+            this.richTextBox1.TabIndex = 26;
+            this.richTextBox1.TabStop = false;
+            this.richTextBox1.Text = " Website: http://www.buculei.com\n GitHub:   https://github.com/ebuculei/eSysInfo\n" +
+    " LinkedIn: https://www.linkedin.com/in/eugen-buculei";
+            this.richTextBox1.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.richTextBox1_LinkClicked);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 31);
+            this.label3.Margin = new System.Windows.Forms.Padding(10, 5, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(389, 20);
+            this.label3.TabIndex = 28;
+            this.label3.Text = "Copyright © 2022 Eugen Buculei. All Rights Reserved.";
+            // 
+            // AboutBox
+            // 
+            this.AcceptButton = this.okButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 235);
+            this.Controls.Add(this.tableLayoutPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AboutBox";
+            this.Padding = new System.Windows.Forms.Padding(14, 14, 14, 14);
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "AboutBox";
+            this.tableLayoutPanel.ResumeLayout(false);
+            this.tableLayoutPanel.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ eSysInfo/SysInfoForm.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections;
+using System.Management;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace eSysInfo
+{
+    public partial class SysInfoForm : Form
+    {
+        private string fnSys;
+        private string fnHard;
+        private string skSys;
+        private string skHard;
+        public SysInfoForm()
+        {
+            InitializeComponent();
+            treeViewSystem.SelectedNode = treeViewSystem.Nodes.Find("Node89", true)[0];
+        }
+
+        private void InsertInfo(string SearchKey, ref ListView lstView, bool AddNullValue)
+        {
+            lstView.Items.Clear();
+            bool pFlgDataExist = false;
+
+            ManagementObjectSearcher Searcher = new ManagementObjectSearcher("SELECT * FROM " + SearchKey);
+
+            try
+            {
+                foreach (ManagementObject mngObj in Searcher.Get())
+                {
+                    pFlgDataExist = true;
+                    ListViewGroup Group;
+                    try
+                    {
+                        Group = lstView.Groups.Add(mngObj["Name"].ToString(), mngObj["Name"].ToString());
+                    }
+                    catch
+                    {
+                        Group = lstView.Groups.Add(mngObj.ToString(), mngObj.ToString());
+                    }
+
+                    if (mngObj.Properties.Count <= 0)
+                    {
+                        //Interaction.MsgBox("No Information Currently Available", MsgBoxStyle.Information, "No Info to Display");
+                        MessageBox.Show("No Information Available", "No Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        return;
+                    }
+
+                    foreach (PropertyData PD in mngObj.Properties)
+                    {
+                        ListViewItem item = new ListViewItem(Group);
+                        if (lstView.Items.Count % 2 != 0)
+                            item.BackColor = Color.White;
+                        else
+                            item.BackColor = Color.WhiteSmoke;
+
+                        item.Text = PD.Name;
+
+                        if (PD.Value != null && PD.Value.ToString() != "")
+                        {
+                            switch (PD.Value.GetType().ToString())
+                            {
+                                case "System.String[]":
+                                    {
+                                        string[] StrMain = (string[])PD.Value;
+
+                                        string strDisplay = "";
+                                        foreach (string st in StrMain)
+                                            strDisplay += st + " ";
+
+                                        item.SubItems.Add(strDisplay);
+                                        break;
+                                    }
+
+                                case "System.UInt16[]":
+                                    {
+                                        ushort[] ShortMain = (ushort[])PD.Value;
+
+                                        string strDisplay = "";
+                                        foreach (ushort st in ShortMain)
+                                            strDisplay += st.ToString() + " ";
+
+                                        item.SubItems.Add(strDisplay);
+                                        break;
+                                    }
+
+                                default:
+                                    {
+                                        item.SubItems.Add(PD.Value.ToString());
+                                        break;
+                                    }
+                            }
+                        }
+                        else if (AddNullValue == true)
+                            //item.SubItems.Add("No Information Available to Display");
+                            item.SubItems.Add("");
+                        else
+                            continue;
+                        lstView.Items.Add(item);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //Interaction.MsgBox(ex.Message, MsgBoxStyle.Exclamation, "Error on Getting Information");
+                MessageBox.Show("Error on Getting Information", ex.Message, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+            if (pFlgDataExist == false)
+            {
+                ListViewGroup Group;
+                Group = lstView.Groups.Add("No Information Available to Display", "No Information Available to Display");
+                ListViewItem item = new ListViewItem(Group);
+                lstView.Items.Add(item);
+            }
+        }
+
+        private void RemoveNullValue(ref ListView lstView)
+        {
+            foreach (ListViewItem item in lstView.Items)
+            {
+                //if (item.SubItems[1].Text == "No Information Available to Display")
+                if (item.SubItems[1].Text == "")
+                    item.Remove();
+            }
+        }
+
+        private void SizeLastColumn(ListView lv)
+        {
+            lv.Columns[lv.Columns.Count - 1].Width = -2;
+        }
+
+        private void listViewSystem_Resize(object sender, EventArgs e)
+        {
+            SizeLastColumn((ListView)sender);
+        }
+
+        private void listViewHardware_Resize(object sender, EventArgs e)
+        {
+            SizeLastColumn((ListView)sender);
+        }
+
+        private void treeViewSystem_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            Cursor = Cursors.WaitCursor;
+            lblStatus.Text = "Operation in progress, please wait...";
+            toolStripProgressBar1.Visible = true;
+            {
+                var withBlock = treeViewSystem;
+                if (!(e.Node.Parent == null))
+                {
+                    toolStripLabelSys.Text = withBlock.SelectedNode.Text;
+                    this.Refresh();
+                    // Child Node
+                    fnSys = withBlock.SelectedNode.Text.Replace(" ", "");
+                    skSys = "Win32_" + withBlock.SelectedNode.Text.Replace(" ", "");
+                    InsertInfo(skSys, ref listViewSystem, chkShowNullValueSystem.Checked);
+                }
+            }
+            Cursor = Cursors.Default;
+            lblStatus.Text = "Ready";
+            toolStripProgressBar1.Visible = false;
+        }
+
+        private void treeViewHardware_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            Cursor = Cursors.WaitCursor;
+            lblStatus.Text = "Operation in progress, please wait...";
+            toolStripProgressBar1.Visible = true;
+            {
+                var withBlock = treeViewHardware;
+                if (!(e.Node.Parent == null))
+                {
+                    toolStripLabelHard.Text = withBlock.SelectedNode.Text;
+                    //this.Refresh();
+                    // Child Node
+                    fnHard = withBlock.SelectedNode.Text.Replace(" ", "");
+                    skHard = "Win32_" + withBlock.SelectedNode.Text.Replace(" ", "");
+                    InsertInfo(skHard, ref listViewHardware, chkShowNullValueHardware.Checked);
+                }
+            }
+            Cursor = Cursors.Default;
+            lblStatus.Text = "Ready";
+            toolStripProgressBar1.Visible = false;
+        }
+
+        private void chkShowNullValueSystem_Click(object sender, EventArgs e)
+        {
+            Cursor = Cursors.WaitCursor;
+            lblStatus.Text = "Operation in progress, please wait...";
+            toolStripProgressBar1.Visible = true;
+            if (listViewSystem.Items.Count > 1)
+            {
+                if (chkShowNullValueSystem.Checked == false)
+                    RemoveNullValue(ref listViewSystem);
+                else
+                    InsertInfo(skSys, ref listViewSystem, chkShowNullValueSystem.Checked);
+            }
+            Cursor = Cursors.Default;
+            lblStatus.Text = "Ready";
+            toolStripProgressBar1.Visible = false;
+        }
+
+        private void chkShowNullValueHardware_Click(object sender, EventArgs e)
+        {
+            Cursor = Cursors.WaitCursor;
+            lblStatus.Text = "Operation in progress, please wait...";
+            toolStripProgressBar1.Visible = true;
+            if (listViewHardware.Items.Count > 1)
+            {
+                if (chkShowNullValueHardware.Checked == false)
+                    RemoveNullValue(ref listViewHardware);
+                else
+                    InsertInfo(skHard, ref listViewHardware, chkShowNullValueHardware.Checked);
+            }
+            Cursor = Cursors.Default;
+            lblStatus.Text = "Ready";
+            toolStripProgressBar1.Visible = false;
+        }
+
+        SaveFileDialog saveFileDialog = new SaveFileDialog
+        {
+            CheckFileExists = false,
+            DefaultExt = "txt",
+            Filter = "Text File|*.txt",
+            SupportMultiDottedExtensions = true
+        };
+
+        private void btnSavetoTxtSystem_Click(object sender, EventArgs e)
+        {
+            if (this.listViewSystem.Items.Count > 1)
+            {
+                saveFileDialog.FileName = fnSys;
+                //saveFileDialog.FileName = skSys;
+                //saveFileDialog.Filter = "Text File|*.txt";
+
+                DialogResult dr = saveFileDialog.ShowDialog();
+
+                if (dr == DialogResult.OK)
+                {
+                    string txtFileName = saveFileDialog.FileName;   // Path   
+                    if (!string.IsNullOrEmpty(txtFileName))
+                    {
+                        using (StreamWriter myWriter = new StreamWriter(txtFileName))
+                        {
+                            foreach (ListViewItem myItem in listViewSystem.Items)
+                                myWriter.WriteLine(myItem.Text + "   :   " + myItem.SubItems[1].Text);
+                            myWriter.Close();
+                            MessageBox.Show("Saved Data to " + txtFileName, "Saved Successfully", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                }
+
+            }
+        }
+
+        private void btnSavetoTxtHardware_Click(object sender, EventArgs e)
+        {
+            if (this.listViewHardware.Items.Count > 1)
+            {
+                saveFileDialog.FileName = fnHard;
+                //saveFileDialog.FileName = skHard;
+                //saveFileDialog.Filter = "Text File|*.txt";
+
+                DialogResult dr = saveFileDialog.ShowDialog();
+
+                if (dr == DialogResult.OK)
+                {
+                    string txtFileName = saveFileDialog.FileName;   // Path   
+                    if (!string.IsNullOrEmpty(txtFileName))
+                    {
+                        using (StreamWriter myWriter = new StreamWriter(txtFileName))
+                        {
+                            foreach (ListViewItem myItem in listViewHardware.Items)
+                                myWriter.WriteLine(myItem.Text + "   :   " + myItem.SubItems[1].Text);
+                            myWriter.Close();
+                            MessageBox.Show("Saved Data to " + txtFileName, "Saved Successfully", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                }
+
+            }
+        }
+
+        private static void ShowAboutBox()
+        {
+            using (AboutBox ab = new AboutBox())
+            {
+                ab.ShowDialog();
+            }
+        }
+        private void btnAboutSys_Click(object sender, EventArgs e)
+        {
+            ShowAboutBox();
+        }
+
+        private void btnAboutHard_Click(object sender, EventArgs e)
+        {
+            ShowAboutBox();
+        }
+    }
+}
